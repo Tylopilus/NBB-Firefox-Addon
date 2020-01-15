ed324c6a3f841f964f0808faa0c6d60a9394a2b3
Helge <elitaer@gmail.com>
2020-01-15 21:45:24 +0100

    initital commit

---
+++ background.js
@@ -0,0 +1,77 @@
+﻿/*
+Handle error for MenuItems
+*/
+const onCreated = () => {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+
+/*
+Create MenuItems
+*/
+browser.runtime.onMessage.addListener((request) => {
+  if(request.message === "updateContextMenu" ){
+    browser.menus.removeAll()
+   
+    let title = "Preise && Verfügbarkeit"
+    browser.menus.create({
+      id: "price",
+      title: title,
+      contexts: ["all"],
+      documentUrlPatterns: ["*://*.notebooksbilliger.de/*"]
+    },onCreated)
+
+    title = "Stamm"
+    browser.menus.create({
+      id: "section_main_data",
+      title: title,
+      contexts: ["all"],
+      documentUrlPatterns: ["*://*.notebooksbilliger.de/*"]
+    },onCreated)
+
+    title = "Artikelbeschreibung"
+    browser.menus.create({
+      id: "section_article_description",
+      title: title,
+      contexts: ["all"],
+      documentUrlPatterns: ["*://*.notebooksbilliger.de/*"]
+    },onCreated)
+
+    title = "Dateien"
+    browser.menus.create({
+      id: "section_files",
+      title: title,
+      contexts: ["all"],
+      documentUrlPatterns: ["*://*.notebooksbilliger.de/*"]
+    },onCreated)
+
+    title = "Eigenschaften"
+    browser.menus.create({
+      id: "section_properties",
+      title: title,
+      contexts: ["all"],
+      documentUrlPatterns: ["*://*.notebooksbilliger.de/*"]
+    },onCreated)
+  }
+  else if(request.message === "removeContextMenu")
+    browser.menus.removeAll()
+})
+
+/*
+On MenuItem selection communicate with content_script
+*/
+browser.menus.onClicked.addListener((info, tab) => {
+  browser.tabs.sendMessage(tab.id, {})
+  .then((response) => {
+    let URL
+    if(info.menuItemId === "price")
+      URL = "https://backend.notebooksbilliger.de/admin/products.php?section=" + info.menuItemId + "&action=show&pID=" + response.productID; 		
+    else
+      URL = "https://backend.notebooksbilliger.de/admin/admin.php/product/physical/edit/"+ response.productID + "#" + info.menuItemId;
+    browser.tabs.create({url: URL});
+	})
+})
---
+++ main.js
@@ -0,0 +1,54 @@
+﻿/*
+Makeing ProductID a class to get 'get' and 'set'
+*/
+const productID = class ProductID {
+	static setElement(e) {
+		this.e = e
+	}
+
+	static getProductID() {
+		//Get productID from Meta-Tag in Product-Detail-Page	
+		const exprMeta = /.*\/products_id\/(\d*)\//
+		const resMeta = exprMeta.exec(document.querySelector("meta[property='og:url']").getAttribute('content'))
+		console.log("resMeta", resMeta)
+		if(resMeta)
+			return resMeta[1]
+		
+		//Get productID from clicked productImage in Listview
+		const exprImg = /.*\/images\/products\/(\d*)\/(\d*)\//
+		const resImg = exprImg.exec(this.e.target.src)
+		console.log("resImg", resImg)
+		if(resImg)
+			return resImg[2]
+		
+		//no match, return null
+		return null
+	}
+}
+
+/*
+handling communication with background.js - resolves promise with productID
+*/
+browser.runtime.onMessage.addListener((request) => {
+	return Promise.resolve({productID: productID.getProductID()})
+})
+
+/*
+generating menu entries upon right click
+*/
+document.addEventListener("mousedown", (e) => {
+	if(e.button !== 2)
+		return false
+	productID.setElement(e)
+
+	if(productID.getProductID() != null)
+		browser.runtime.sendMessage({
+			message: "updateContextMenu"
+		})
+	else {
+		browser.runtime.sendMessage({
+			message: "removeContextMenu"
+		})
+	}
+	return false
+}, true)
